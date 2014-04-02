3c6bba8be80cf335eb4993f728e699727d04754a
ilturco <salvatore.passerini@gmail.com>
2014-04-02 14:41:29 +0100

    first commit

---
+++ src/main/java/com/packtpub/Calculator.java
@@ -0,0 +1,11 @@
+package com.packtpub;
+
+/**
+ * Created by salvatore on 02/04/2014.
+ */
+public class Calculator {
+    public int sum(int x, int y)
+    {
+        return x + y;
+    }
+}
---
+++ src/test/java/com/packtpub/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.packtpub;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Created by salvatore on 02/04/2014.
+ */
+public class CalculatorTest extends TestCase {
+    public void testSum() throws Exception {
+        Calculator calculator = new Calculator();
+        int sum = calculator.sum(1, 2);
+        Assert.assertEquals(3, sum);
+
+    }
+}
